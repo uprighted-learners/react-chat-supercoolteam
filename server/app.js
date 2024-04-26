520157091f350c653935f86b23441057371376e9
RyanF8 <Ryanfish4229@gmail.com>
2024-04-26 19:09:29 -0400

    working create user and login user endpoint, data loads into mongodb as well so everything looks good

--- server/app.js
+++ server/app.js
@@ -2,8 +2,15 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 
+//route endpoint imports
+import userRoutes from './routes/userRoutes.js'
+
 const app = express();
 dotenv.config();
+
+app.get('/health/test', (req, res) => {
+    res.send('healthy server')
+});
 
 const PORT = process.env.PORT || 6000;
 const MONGOURL = process.env.MONGO_URL;
@@ -19,3 +26,5 @@ mongoose.connect(MONGOURL).then(() => {
 //server middleware
 app.use(express.json())
 
+//call endpoint routes
+app.use(userRoutes);
