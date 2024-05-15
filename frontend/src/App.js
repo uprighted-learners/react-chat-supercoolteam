97d566f90286c468c427335fdeae68f3fedf0624
Quinn <qlansbury14@gmail.com>
2024-05-15 18:39:03 -0400

    Newest commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import Auth from './components/Auth'
+import React from "react";
+import createMessage from "./pages/createMessage";
+import Auth from "./pages/Auth";
 
 export default function App() {
   return (
-    <>
-    <Auth />
-    </>
-  )
-}
-
+    <createMessage></createMessage>
+//     <Router>
+//       <div>
+//         <Navigation>
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route
+//               path="/createMessage"
+//               element={
+//                 <RequireAuth>
+//                   <createMessage />
+//                 </RequireAuth>
+//               }
+//             />
+//           </Routes>
+//         </Navigation>
+//       </div>
+//     </Router>
+//   );
+// }
+  )}
