c63c59cd3bdca38e32923814714050a927b97ada
Quinn <qlansbury14@gmail.com>
2024-05-15 19:17:43 -0400

    app.js changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,23 @@
 import React from "react";
 import createMessage from "./pages/createMessage";
 import Auth from "./pages/Auth";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 export default function App() {
   return (
-    <createMessage></createMessage>
-//     <Router>
-//       <div>
-//         <Navigation>
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/login" element={<Login />} />
-//             <Route
-//               path="/createMessage"
-//               element={
-//                 <RequireAuth>
-//                   <createMessage />
-//                 </RequireAuth>
-//               }
-//             />
-//           </Routes>
-//         </Navigation>
-//       </div>
-//     </Router>
-//   );
-// }
-  )}
+    <Router>
+      <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Auth />} />
+            <Route
+              path="/createMessage"
+              element={<createMessage />}
+            />
+          </Routes>
+      </div>
+    </Router>
+  );
+}
